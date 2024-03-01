26021fc68cad8c480a051c2b90a9095e7df0310e
mingmingsal <miguellorenzosalvador@gmail.com>
2024-03-01 12:04:20 +0800

    initial commit

---
+++ src/ToDo.js
@@ -0,0 +1,10 @@
+class ToDo{
+    constructor (title,description,dueDate,priority,notes,checklist){
+        this.title=title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+        this.checklist=checklist;
+    }
+}
